4a15004dc82fc69d8884b5155fa91a540d3fd378
Hans Felde <m33pfish@gmail.com>
2015-05-18 23:13:05 -0500

    add some stuff

--- render.js
+++ render.js
@@ -3,3 +3,21 @@ canvas.id = "canvas";
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 document.body.appendChild("canvas");
+
+var time = 0;
+
+var control = function() {
+  
+};
+
+var render = function() {
+  
+};
+
+var main = function() {
+  control();
+  render();
+  
+  time += 1 / 60;
+  requestAnimationFrame(main);
+};
